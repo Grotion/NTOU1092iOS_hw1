2d07829f787bb4daa7ac711cf02fce9e775b2fb0
GroTion <grotion0720@gmail.com>
2021-03-03 16:11:21 +0800

    Initial Commit

---
+++ ntou1092iOS_hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ntou1092iOS_hw1
+//
+//  Created by Shaun Ku on 2021/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ntou1092iOS_hw1/ntou1092iOS_hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  ntou1092iOS_hw1App.swift
+//  ntou1092iOS_hw1
+//
+//  Created by Shaun Ku on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct ntou1092iOS_hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
